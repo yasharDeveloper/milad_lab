3297a5ac6d2d1a9cb8b1c8fb96ea94d1838d439d
Yashar <yasharfooladgar204@gmail.com>
2024-08-11 07:31:04 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/partials/Header.vue'
+// import Sidebar from './components/partials/Sidebar.vue'
+// import Footer from './components/partials/Footer.vue'
+
+</script>
+
+<template>
+  <header> 
+    
+    <Header></Header>
+
+    
+
+
+  </header>
+
+
+    
+
+  
+
+
+
+
+
+
+  <main>
+   
+  </main>
+</template>
+
+<style>
+  @import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+  @import "../node_modules/apexcharts/dist/apexcharts.css";
+  @import "../node_modules/quill/dist/quill.snow.css";
+  @import "../node_modules/animate.css";
+  @import "../node_modules/sweetalert2/dist/sweetalert2.css";
+  @import "../node_modules/nouislider/dist/nouislider.css";
+  @import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+  @import "../node_modules/socicon/css/socicon.css";
+  @import "../node_modules/line-awesome/dist/line-awesome/css/line-awesome.css";
+  @import "../node_modules/dropzone/dist/dropzone.css";
+  @import "../node_modules/@vueform/multiselect/themes/default.css";
+  @import "../node_modules/prism-themes/themes/prism-shades-of-purple.css";
+  @import "../node_modules/element-plus/dist/index.css";
+
+
+
+  @import "assets/css/style.bundle.rtl.css";
+  @import "assets/css/fullcalendar.bundle.css";
+  @import "assets/css/plugins.bundle.rtl.css";
+  @import "assets/css/datatables.bundle.css";
+  @import "https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700";
+
+</style>
